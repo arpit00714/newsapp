d17c010f164e8241d13381cd48616f0227a3524e
arpit00714 <arpitkhare14@gmail.com>
2023-05-23 15:17:41 +0530

    First_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+         <NavBar />
+         <Routes>
+          <Route exact path="/" element={<News Key="general" pageSize={5} country="in" category="general"/>}/>
+          <Route exact path="/business" element={<News Key="business" pageSize={5} country="in" category="business"/>}/>
+          <Route exact path="/entertainment" element={<News Key="entertainment" pageSize={5} country="in" category="entertainment"/>}/>
+          <Route exact path="/general" element={<News Key="general" pageSize={5} country="in" category="general"/>}/>
+          <Route exact path="/health" element={<News Key="health" pageSize={5} country="in" category="health"/>}/>
+          <Route exact path="/science" element={<News Key="science" pageSize={5} country="in" category="science"/>}/>
+          <Route exact path="/sport" element={<News Key="sport" pageSize={5} country="in" category="sport"/>}/>
+          <Route exact path="/technology" element={<News Key="technology" pageSize={5} country="in" category="technology"/>}/>
+           </Routes>
+      </Router>
+      </div>
+    )
+  }
+}
